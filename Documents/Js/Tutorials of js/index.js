fa9f9a7cfe6d6f7cd3f923ab24793f889cf08ca9
Mukes-h-Programmer <midasmukesh6@gmail.com>
2022-02-26 21:31:14 +0530

    Variables 26 FEB

--- Documents/Js/Tutorials of js/index.js
+++ Documents/Js/Tutorials of js/index.js
@@ -11,3 +11,11 @@
     // console.clear();
     // console.assert(4==6);//getting errors
     console.assert(4==4);//doesn't get in any error
+    // JavaScript Variables
+// What are variables ? - Containers to store data values
+
+var m = 34;
+var n = 34;
+
+console.log(m + n);
+document.write("I am a lol");
---
+++ Documents/Js/Variables/index.js
@@ -0,0 +1,109 @@
+// // JavaScript : -> Variables
+// // What  are Varibales ? -  Containers to store data values
+
+// var n1 = 30;
+// var n2 = 3409;
+
+// console.log(n1 + n2);
+
+// // 4. Data types in JavaScript
+// // Numbers
+// var m1 = 3298;
+// var m2 = 349;
+
+// //String
+//  var str1 = "this is me";
+//  var str2 = 'this also me ';
+
+//  // objects 
+//  var marks = {
+//      ravi: 234,
+//      mukesh : 239,
+//      shubham: 3489
+
+//  }
+//  console.log(marks);
+
+//  // Booleans
+//  var a = true;
+//  var b = false;
+
+//  console.log(a,b);
+
+// //  var und = undefined;
+// //  console.log(undefined);
+// // console.log(und);
+
+// // var und;
+// // console.log(und);
+
+// // var n = null;
+// // console.log(n);
+
+// /*At a very high level, there are two types of 
+// data types in Java script
+
+// 1. Primitve data types: undefined, null, number, string, boolean, symbol
+// 2. Reference data types: Arrays and Objects 
+// */
+
+// var arr = [1, 2, "Mukesh", 4, 5];
+// console.log(arr[3]);
+
+// console.log(arr[2]);
+
+// console.log(arr);
+
+// // Operaters in java
+// // Arithmetic Operators
+// var a = 4;
+// var b = 2;
+
+// console.log("The value of a + b = ", a+b );
+// console.log("The value of a - b = ", a-b );
+// console.log("The value of a * b = ", a*b );
+// console.log("The value of a / b = ", a/b );
+// console.log("The value of a % b = ", a%b );
+
+// // Assignment Operators
+
+// var c = b;
+// c+=2;
+// c-=2;
+// c*=2;
+// c/=2;
+// c%=2;
+
+// console.log("Last values of c" + c);
+
+// // Comparison Operators
+
+// var x = 434;
+// var y = 39;
+
+//  console.log(x == y);
+//  console.log(x >= y);
+//  console.log(x <= y);
+//  console.log(x > y);
+//  console.log(x < y);
+
+ // Logical Operators
+// Logical AND
+
+//  console.log(true && true); 
+//  console.log(true && false);
+//  console.log(false && true);
+//  console.log(false && false);
+
+// Logical OR
+//  console.log(true  || true); 
+//  console.log(true  || false);
+//  console.log(false || true);
+//  console.log(false || false);
+
+// Logical NOT
+
+
+console.log(!false ); 
+console.log(!true );
+
