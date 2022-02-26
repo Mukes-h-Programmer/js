b2102bd0bb08ede1296758d50bd3ad6540ab68da
Mukes-h-Programmer <midasmukesh6@gmail.com>
2022-02-26 19:14:57 +0530

    26 FEB

---
+++ Documents/Js/Tutorials of js/index.js
@@ -0,0 +1,13 @@
+
+// 1. Ways to print in JavaScript
+    // console.log("Hello World");
+    //  alert("me");
+    // document.write("This is document write.");
+
+    // 2. JavaScript console application programming interface (API)
+    console.log("Hello world");
+    console.warn("this is warning");
+    console.error("I am error");
+    // console.clear();
+    // console.assert(4==6);//getting errors
+    console.assert(4==4);//doesn't get in any error
